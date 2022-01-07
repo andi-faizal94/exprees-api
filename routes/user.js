df884b75fe80a0ce562d65197a71ed539b4dd8ed
Andi Faizal <andifaizal647@gmail.com>
2022-01-07 22:57:18 +0700

    add post user and recreat respon and request

--- routes/user.js
+++ routes/user.js
@@ -3,22 +3,34 @@ import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
+let users = [
+  //   {
+  //     firstName: "andi",
+  //     lastName: "faizal",
+  //     age: 25,
+  //   },
+  //   {
+  //     firstName: "zain",
+  //     lastName: "malik",
+  //     age: 12,
+  //   },
+];
+
 router.get("/", (req, res) => {
-  res.send("my name andi");
+  res.send(users);
 });
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  console.log(id);
-  res.send("you");
+  //   const id = uuidv4();
+
+  res.send("hallo");
 });
 
-router.post("/", (res, req) => {
-  const user = req.params;
-  const id = uuidv4();
-  console.log(id);
-  res.send("aa");
-  //   res.send(user);
+router.post("/", (req, res) => {
+  const user = req.body;
+  users.push({ ...user, id: uuidv4 });
+  res.send(`User with name  `);
 });
 
 export default router;
