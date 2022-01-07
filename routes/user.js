a97519e0f85a06ec2c704edfe8be2db15dd1513a
Andi Faizal <andifaizal647@gmail.com>
2022-01-07 23:29:14 +0700

    add crud

--- routes/user.js
+++ routes/user.js
@@ -22,15 +22,35 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  //   const id = uuidv4();
-
-  res.send("hallo");
+  const findUser = users.find((user) => user.id === id);
+  res.send(findUser);
 });
 
 router.post("/", (req, res) => {
   const user = req.body;
-  users.push({ ...user, id: uuidv4 });
-  res.send(`User with name  `);
+  users.push({ ...user, id: uuidv4() });
+  res.send(`User with name ${users} `);
 });
 
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, age } = req.body;
+  const user = users.find((user) => user.id === id);
+  if (firstName) {
+    user.firstName = firstName;
+  }
+  if (lastName) {
+    user.lastName = lastName;
+  }
+  if (age) {
+    user.age = age;
+  }
+  res.send(`user updated `);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  users.filter((user) => user.id !== id);
+  res.send(`user deleted`);
+});
 export default router;
